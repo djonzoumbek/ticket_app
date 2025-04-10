8965041d4a78647f7a2f7bea7c4ecbdcc49c26c6
djonzoumbek <guepelbekamis@gmail.com>
2025-04-10 01:46:28 +0000

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text("My Flutter real 1rs app"),
+        ) ,
+        body: Center(child: Text("Hello Flutter"),),
+      ),
+    );
+  }
+}
